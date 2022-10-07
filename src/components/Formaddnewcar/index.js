35b1f34291b88529aeaecc52e67824b2f823ec6d
Kukuh Ferlanda <ferlandakukuh@gmail.com>
2022-10-07 21:13:12 +0700

    edit form

--- src/components/Formaddnewcar/index.js
+++ src/components/Formaddnewcar/index.js
@@ -12,7 +12,7 @@ const Formaddnewcar = () => {
         <p>breadcrumb</p>
       </div>
       <div>
-        <p>Add New Car</p>
+        <h3>Add New Car</h3>
       </div>
       <div>
       <Form>
@@ -25,209 +25,104 @@ const Formaddnewcar = () => {
           </Label>
             <Col sm={10}>
               <Input
-                id="exampleEmail"
-                name="email"
-                placeholder="with a placeholder"
-                type="email"
+                id="Input Mobil"
+                name="Input"
+                placeholder="Input Nama/Tipe Mobil"
+                type="Text"
               />
             </Col>
         </FormGroup>
         <FormGroup row>
           <Label
-            for="examplePassword"
+            for="Harga"
             sm={2}
           >
-            Password
+            Harga
           </Label>
             <Col sm={10}>
               <Input
-                name="password"
-                placeholder="password placeholder"
+                name="Harga"
+                placeholder="Input Harga Sewa Mobil"
               />
             </Col>
         </FormGroup>
-  <FormGroup row>
-    <Label
-      for="exampleSelect"
-      sm={2}
-    >
-      Select
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleSelect"
-        name="select"
-        type="select"
-      >
-        <option>
-          1
-        </option>
-        <option>
-          2
-        </option>
-        <option>
-          3
-        </option>
-        <option>
-          4
-        </option>
-        <option>
-          5
-        </option>
-      </Input>
-    </Col>
-  </FormGroup>
-  <FormGroup row>
-    <Label
-      for="exampleSelectMulti"
-      sm={2}
-    >
-      Select Multiple
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleSelectMulti"
-        multiple
-        name="selectMulti"
-        type="select"
-      >
-        <option>
-          1
-        </option>
-        <option>
-          2
-        </option>
-        <option>
-          3
-        </option>
-        <option>
-          4
-        </option>
-        <option>
-          5
-        </option>
-      </Input>
-    </Col>
-  </FormGroup>
-  <FormGroup row>
-    <Label
-      for="exampleText"
-      sm={2}
-    >
-      Text Area
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleText"
-        name="text"
-        type="textarea"
-      />
-    </Col>
-  </FormGroup>
-  <FormGroup row>
-    <Label
-      for="exampleFile"
-      sm={2}
-    >
-      File
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleFile"
-        name="file"
-        type="file"
-      />
-      <FormText>
-        This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
-      </FormText>
-    </Col>
-  </FormGroup>
-  <FormGroup
-    row
-    tag="fieldset"
-  >
-    <legend className="col-form-label col-sm-2">
-      Radio Buttons
-    </legend>
-    <Col sm={10}>
-      <FormGroup check>
-        <Input
-          name="radio2"
-          type="radio"
-        />
-        {' '}
-        <Label check>
-          Option one is this and that—be sure to include why it‘s great
-        </Label>
-      </FormGroup>
-      <FormGroup check>
-        <Input
-          name="radio2"
-          type="radio"
-        />
-        {' '}
-        <Label check>
-          Option two can be something else and selecting it will deselect option one
-        </Label>
-      </FormGroup>
-      <FormGroup
-        check
-        disabled
-      >
-        <Input
-          disabled
-          name="radio2"
-          type="radio"
-        />
-        {' '}
-        <Label check>
-          Option three is disabled
-        </Label>
-      </FormGroup>
-    </Col>
-  </FormGroup>
-  <FormGroup row>
-    <Label
-      for="checkbox2"
-      sm={2}
-    >
-      Checkbox
-    </Label>
-    <Col
-      sm={{
-        size: 10
-      }}
-    >
-      <FormGroup check>
-        <Input
-          id="checkbox2"
-          type="checkbox"
-        />
-        {' '}
-        <Label check>
-          Check me out
-        </Label>
-      </FormGroup>
-    </Col>
-  </FormGroup>
-  <FormGroup
-    check
-    row
-  >
-    <Col
-      sm={{
-        offset: 2,
-        size: 10
-      }}
-    >
-      <Button>
-        Submit
-      </Button>
-    </Col>
-  </FormGroup>
-</Form>
-      </div>
-    </div>
+        <FormGroup row>
+          <Label
+            for="Input Foto"
+            sm={2}
+          >
+            Foto
+          </Label>
+            <Col sm={10}>
+              <Input
+                id="Input Foto"
+                name="Foto"
+                placeholder="Upload Foto Mobil"
+                type="File"
+              />
+                <FormText>
+                  File size max. 2MB
+                </FormText>
+            </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label
+            for="exampleSelect"
+            sm={2}
+          >
+            Kategori
+          </Label>
+            <Col sm={10}>
+              <Input
+                id="Kategori"
+                name="select"
+                placeholder="Pilih Kategori Mobil"
+                type="select"
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label
+            for="exampleText"
+            sm={2}
+          >
+          Text Area
+          </Label>
+            <Col sm={10}>
+              <Input
+                id="exampleText"
+                name="text"
+                type="textarea"
+              />
+            </Col>
+        </FormGroup>
+        <FormGroup
+          check
+          row
+        >
+          <Col
+            sm={{
+            offset: 2,
+            size: 10
+            }}
+          >
+            <Button>
+              Cancel
+            </Button>
+            <Button>
+              Submit
+            </Button>
+          </Col>
+        </FormGroup>
+    </Form>
+  </div>
+</div>
     )
 }
 
