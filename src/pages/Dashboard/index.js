151fec51c5140eb7153bf699300d34c9c53a7a1f
yusakman <yusakmanullang93@gmail.com>
2022-10-20 14:37:04 +0800

    Add Table Component to Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,7 @@ import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import CarsChart from "../../components/CarsChart";
 import SectionTitle from "../../components/SectionTitle";
 import NavbarAdmin from "../../components/NavbarAdmin";
+import Table from "../../components/Table"
 
 const Dashboard = () => {
   const page = "dashboard";
@@ -28,6 +29,7 @@ const Dashboard = () => {
         <div className="content">
           <SectionTitle title={"Rental Car Data Visualization"} />
           <CarsChart />
+          <Table />
         </div>
       </main>
     </div>
