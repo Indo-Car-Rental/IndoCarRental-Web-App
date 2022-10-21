54467c1c20c0774c43caa4a768943da9068cb7f7
Hafizh Faza Nugraha <hafizhfazanugraha@gmail.com>
2022-10-21 18:48:23 +0700

    delete redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import dataReducer from "./dataReducer";
 import sideBarReducer from "./sideBarReducers";
+import carReducer from "./carReducer";
 
 const rootReducer = combineReducers({
   dataOrder: dataReducer,
