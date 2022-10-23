897d9a8bc27ad8c84b93636fcfb30a0a33e02508
Kukuh Ferlanda <ferlandakukuh@gmail.com>
2022-10-23 21:03:55 +0700

    axios

--- src/components/Formaddnewcar/index.js
+++ src/components/Formaddnewcar/index.js
@@ -1,25 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Label, Input, FormGroup, Form, FormText, Col, Placeholder } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Formaddnewcar/style.css'
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import NavbarAdmin from "../../components/NavbarAdmin";
+import SectionTitle from "../../components/SectionTitle";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Formaddnewcar = () => {
+  const { sideBar } = useSelector((state) => state);
+  const page = "cars"
+  const [namaMobil, setNamaMobil] = useState("");
+  const [hargaMobil, setHargaMobil] = useState("");
+  const [fotoMobil, setFotoMobil] = useState("");
+  const [kategoriMobil, setKategoriMobil] = useState("");
+  const [save, setSave] = useState('')
+
+  const props = {
+    page,
+  };
+  
+  const handleNamaMobil = (e) =>{
+    setNamaMobil(e.target.value);
+}
+
+const handleHargaMobil = (e) =>{
+  setHargaMobil(e.target.value);
+}
+
+const handleFotoMobil = (e) =>{
+  console.log(fotoMobil)
+  setFotoMobil(e.target.files[0]);
+}
+
+const handleKategoriMobil = (e) =>{
+  setKategoriMobil(e.target.value);
+}
+
+const handleAddNewCar = (e) =>{
+  e.preventDefault(namaMobil, hargaMobil, fotoMobil, kategoriMobil);
+  let formData = new FormData();
+  formData.append('image', fotoMobil);
+  const payload = {
+    namaMobil,
+    hargaMobil,
+    formData,
+    kategoriMobil
+  }
+  axios({
+    method: 'POST',
+    url: 'https://bootcamp-rent-cars.herokuapp.com/admin/car',
+    data: {
+      name : payload.namaMobil,
+      category : payload.hargaMobil,
+      price : payload.kategoriMobil,
+      status : false,
+      image : payload.formData
+    },
+    headers: {
+        access_token : localStorage.getItem("admin_token")
+    }
+  })
+  .then(function (res) {
+    console.log(res)
+  })
+  .catch(function (error) {
+    console.log(error)
+  });
+}
 
   return (
     <div>
+      <NavbarAdmin {...props} />
+      <main className={!!sideBar.hideSideBar === true ? "fullwidth" : ""}>
+        <div className="breadcrumb-wrapper">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/admin/cars">Cars</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/admin/cars">List Car</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Add New Car</BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <div className="content">
+        <SectionTitle title={"Add New Car"} />
       <div>
-        <nav aria-label="breadcrumbs" class="rank-math-breadcrumb">
-          <a href="" class="cars">Cars</a>
-          <span class="separator"> > </span>
-          <a href="" class="list-car">List Car</a>
-          <span class="separator"> > </span>
-          <span class="last">Add New Car</span>
-        </nav>
-      </div>
-      <div class="add-new-car">
-        <p>Add New Car</p>
-      </div>
-      <div>
+      {!!save && <div className="alert alert-success">Berhasil Ditambah</div>}
         <Form>
           <FormGroup row>
             <Label
@@ -34,6 +104,7 @@ const Formaddnewcar = () => {
                   name="Input"
                   placeholder="Input Nama/Tipe Mobil"
                   type="Text"
+                  onChange={(e) => handleNamaMobil(e)}
                 />
               </Col>
           </FormGroup>
@@ -48,6 +119,7 @@ const Formaddnewcar = () => {
                 <Input
                   name="Harga"
                   placeholder="Input Harga Sewa Mobil"
+                  onChange={(e) => handleHargaMobil(e)}
                 />
               </Col>
           </FormGroup>
@@ -64,6 +136,7 @@ const Formaddnewcar = () => {
                   name="Foto"
                   placeholder="Upload Foto Mobil"
                   type="File"
+                  onChange={(e) => handleFotoMobil(e)}
                 />
                   <FormText>
                     File size max. 2MB
@@ -83,6 +156,7 @@ const Formaddnewcar = () => {
                   name="select"
                   placeholder="Pilih Kategori Mobil"
                   type="select"
+                  onChange={(e) => handleKategoriMobil(e)}
                 >
                   <option>2-4 people</option>
                   <option>4-6 people</option>
@@ -112,14 +186,16 @@ const Formaddnewcar = () => {
                 <p>-</p>
               </Col>
           </FormGroup>
-              <Button className="cancel">
-                Cancel
-              </Button>
-              <Button className="save">
+                <Button className="cancel" onclick="history.back()">
+                  Cancel
+                </Button>
+              <Button className="save" onClick={handleAddNewCar}>
                 Save
-              </Button>
+              </Button>  
         </Form>
       </div>
+        </div>
+      </main>
     </div>
     )
 }
