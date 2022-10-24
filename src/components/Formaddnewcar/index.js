a7dca7934a85d7ffe98fcd642e36803d69823b0e
yusakman <yusakmanullang93@gmail.com>
2022-10-24 14:31:05 +0800

    Add example

--- src/components/Formaddnewcar/index.js
+++ src/components/Formaddnewcar/index.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import { Button, Label, Input, FormGroup, Form, FormText, Col, Placeholder } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Formaddnewcar/style.css'
+import {
+  Button,
+  Label,
+  Input,
+  FormGroup,
+  Form,
+  FormText,
+  Col,
+  Placeholder,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Formaddnewcar/style.css";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 import NavbarAdmin from "../../components/NavbarAdmin";
 import SectionTitle from "../../components/SectionTitle";
@@ -11,65 +20,75 @@ import axios from "axios";
 
 const Formaddnewcar = () => {
   const { sideBar } = useSelector((state) => state);
-  const page = "cars"
+  const page = "cars";
   const [namaMobil, setNamaMobil] = useState("");
   const [hargaMobil, setHargaMobil] = useState("");
   const [fotoMobil, setFotoMobil] = useState("");
   const [kategoriMobil, setKategoriMobil] = useState("");
-  const [save, setSave] = useState('')
+  const [save, setSave] = useState("");
+  const [error, setError] = useState("");
 
   const props = {
     page,
   };
-  
-  const handleNamaMobil = (e) =>{
+
+  const handleNamaMobil = (e) => {
     setNamaMobil(e.target.value);
-}
+  };
 
-const handleHargaMobil = (e) =>{
-  setHargaMobil(e.target.value);
-}
+  const handleHargaMobil = (e) => {
+    setHargaMobil(e.target.value);
+  };
 
-const handleFotoMobil = (e) =>{
-  console.log(fotoMobil)
-  setFotoMobil(e.target.files[0]);
-}
+  const handleFotoMobil = (e) => {
+    setFotoMobil(e.target.files[0]);
+  };
 
-const handleKategoriMobil = (e) =>{
-  setKategoriMobil(e.target.value);
-}
-
-const handleAddNewCar = (e) =>{
-  e.preventDefault(namaMobil, hargaMobil, fotoMobil, kategoriMobil);
-  let formData = new FormData();
-  formData.append('image', fotoMobil);
-  const payload = {
-    namaMobil,
-    hargaMobil,
-    formData,
-    kategoriMobil
-  }
-  axios({
-    method: 'POST',
-    url: 'https://bootcamp-rent-cars.herokuapp.com/admin/car',
-    data: {
-      name : payload.namaMobil,
-      category : payload.hargaMobil,
-      price : payload.kategoriMobil,
-      status : false,
-      image : payload.formData
-    },
-    headers: {
-        access_token : localStorage.getItem("admin_token")
+  const handleKategoriMobil = (e) => {
+    if (e.target.value === "2-4 people") {
+      setKategoriMobil("small");
+    } else if (e.target.value === "4-6 people") {
+      setKategoriMobil("medium");
+    } else {
+      setKategoriMobil("large");
     }
-  })
-  .then(function (res) {
-    console.log(res)
-  })
-  .catch(function (error) {
-    console.log(error)
-  });
-}
+  };
+
+  const handleAddNewCar = (e) => {
+    e.preventDefault(namaMobil, hargaMobil, fotoMobil, kategoriMobil);
+
+    let formData = new FormData();
+    formData.append("image", fotoMobil);
+    formData.append("name", fotoMobil.name);
+
+    const payload = {
+      namaMobil,
+      hargaMobil,
+      formData,
+      kategoriMobil,
+    };
+    axios({
+      method: "POST",
+      url: "https://bootcamp-rent-cars.herokuapp.com/admin/car",
+      data: {
+        name: payload.namaMobil,
+        category: payload.kategoriMobil,
+        price: payload.hargaMobil,
+        status: false,
+        image: payload.formData,
+      },
+      headers: {
+        access_token: localStorage.getItem("admin-token"),
+      },
+    })
+      .then(function (res) {
+        setSave(res.status);
+        setError(null)
+      })
+      .catch(function (error) {
+        setError(error);
+      });
+  };
 
   return (
     <div>
@@ -87,117 +106,98 @@ const handleAddNewCar = (e) =>{
           </Breadcrumb>
         </div>
         <div className="content">
-        <SectionTitle title={"Add New Car"} />
-      <div>
-      {!!save && <div className="alert alert-success">Berhasil Ditambah</div>}
-        <Form>
-          <FormGroup row>
-            <Label
-              for="Nama/Tipe Mobil"
-              sm={2}
-            >
-              Nama/Tipe Mobil
-            </Label>
-              <Col sm={10}>
-                <Input
-                  id="Input Mobil"
-                  name="Input"
-                  placeholder="Input Nama/Tipe Mobil"
-                  type="Text"
-                  onChange={(e) => handleNamaMobil(e)}
-                />
-              </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label
-              for="Harga"
-              sm={2}
-            >
-              Harga
-            </Label>
-              <Col sm={10}>
-                <Input
-                  name="Harga"
-                  placeholder="Input Harga Sewa Mobil"
-                  onChange={(e) => handleHargaMobil(e)}
-                />
-              </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label
-              for="Input Foto"
-              sm={2}
-            >
-              Foto
-            </Label>
-              <Col sm={10}>
-                <Input
-                  id="Input Foto"
-                  name="Foto"
-                  placeholder="Upload Foto Mobil"
-                  type="File"
-                  onChange={(e) => handleFotoMobil(e)}
-                />
-                  <FormText>
-                    File size max. 2MB
-                  </FormText>
-              </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label
-              for="Select"
-              sm={2}
-            >
-              Kategori
-            </Label>
-              <Col sm={10}>
-                <Input
-                  id="Kategori"
-                  name="select"
-                  placeholder="Pilih Kategori Mobil"
-                  type="select"
-                  onChange={(e) => handleKategoriMobil(e)}
-                >
-                  <option>2-4 people</option>
-                  <option>4-6 people</option>
-                  <option>6-8 people</option>
-                </Input>
-              </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label
-              for="exampleText"
-              sm={2}
-            >
-              Created at
-            </Label>
-              <Col sm={10}>
-                <p>-</p>
-              </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label
-              for="exampleText"
-              sm={2}
-            >
-              Updated at
-            </Label>
-              <Col sm={10}>
-                <p>-</p>
-              </Col>
-          </FormGroup>
-                <Button className="cancel" onclick="history.back()">
-                  Cancel
-                </Button>
+          <SectionTitle title={"Add New Car"} />
+          <div>
+            <Form>
+              <FormGroup row>
+                <Label for="Nama/Tipe Mobil" sm={2}>
+                  Nama/Tipe Mobil
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    id="Input Mobil"
+                    name="Input"
+                    placeholder="Input Nama/Tipe Mobil"
+                    type="Text"
+                    onChange={(e) => handleNamaMobil(e)}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="Harga" sm={2}>
+                  Harga
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    name="Harga"
+                    placeholder="Input Harga Sewa Mobil"
+                    onChange={(e) => handleHargaMobil(e)}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="Input Foto" sm={2}>
+                  Foto
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    id="Input Foto"
+                    name="Foto"
+                    placeholder="Upload Foto Mobil"
+                    type="File"
+                    onChange={(e) => handleFotoMobil(e)}
+                  />
+                  <FormText>File size max. 2MB</FormText>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="Select" sm={2}>
+                  Kategori
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    id="Kategori"
+                    name="select"
+                    placeholder="Pilih Kategori Mobil"
+                    type="select"
+                    onChange={(e) => handleKategoriMobil(e)}
+                  >
+                    <option>2-4 people</option>
+                    <option>4-6 people</option>
+                    <option>6-8 people</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="exampleText" sm={2}>
+                  Created at
+                </Label>
+                <Col sm={10}>
+                  <p>-</p>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="exampleText" sm={2}>
+                  Updated at
+                </Label>
+                <Col sm={10}>
+                  <p>-</p>
+                </Col>
+                {!!save && <div className="alert alert-success">Berhasil Ditambah</div>}
+                {!!error && <div className="alert alert-danger">Error</div>}
+              </FormGroup>
+              <Button className="cancel" onclick="history.back()">
+                Cancel
+              </Button>
               <Button className="save" onClick={handleAddNewCar}>
                 Save
-              </Button>  
-        </Form>
-      </div>
+              </Button>
+            </Form>
+          </div>
         </div>
       </main>
     </div>
-    )
-}
+  );
+};
 
 export default Formaddnewcar;
