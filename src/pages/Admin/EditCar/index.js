0b848d49f96226162d8a1d7ed256ab96e8b41c2c
Dwi Pamungkas <dw.pamungkas@gmail.com>
2022-10-24 21:50:23 +0700

    update edit-car

--- src/pages/Admin/EditCar/index.js
+++ src/pages/Admin/EditCar/index.js
@@ -21,10 +21,10 @@ const EditCar = () => {
                 <div className="breadcrumb-wrapper">
                     <Breadcrumb>
                         <BreadcrumbItem>
-                            <a href="dashboard">Dashboard</a>
+                            <a href="/admin/dashboard">Dashboard</a>
                         </BreadcrumbItem>
                         <BreadcrumbItem>
-                            <a href="cars">Cars</a>
+                            <a href="/admin/cars">Cars</a>
                         </BreadcrumbItem>
                         <BreadcrumbItem active>Edit Car</BreadcrumbItem>
                     </Breadcrumb>
@@ -34,7 +34,6 @@ const EditCar = () => {
                     <AdminFormCarEdit />
                 </div>
             </main>
-
         </div>
     );
 }
